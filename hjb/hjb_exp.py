87210fed74a0e74f01fd2298fe14511074d946ec
Mathias Oster <oster@desargues.math.tu-berlin.de>
2021-11-18 10:22:17 +0100

    updated open loop

--- hjb/hjb_exp.py
+++ hjb/hjb_exp.py
@@ -6,63 +6,76 @@ Created on Fri Nov 12 09:55:05 2021
 @author: goette
 """
 
-import os, sys
+# , hermite_measures
+from misc import random_homogenous_polynomial_sum,  legendre_measures, Gramian, HkinnerLegendre
+import ode
+import copy
+import numpy as np
+from als import ALS
+import optimize
+from tilde_r import calc_tilde_r, calc_total_reward
+import os
+import sys
+import set_dynamics
+
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
 
-from tilde_r import calc_tilde_r
-import optimize
-from misc import random_homogenous_polynomial_sum,  legendre_measures, Gramian, HkinnerLegendre  #, hermite_measures
-from als import ALS
-import numpy as np
-import copy
-
 data = np.load('data.npy')
 t_vec = np.load("t_vec_p.npy")
-
+T = t_vec[-1]
+load_me = np.load('data.npy')
+tau = load_me[3]#t_vec[1]-t_vec[0]
+print(tau)
 order = int(data[4])
-degree = 7
+degree = 3
 maxGroupSize = 2
-maxSweeps = 8
-tol =1e-4
-maxPolIt = 3
-
+maxSweeps = 2
+tol = 1e-4
+maxPolIt = 1
+Schloegel_ode = ode.Ode()
 print(f"Order {order}")
 print(f"degree {degree}")
 
-#generate sample data
-trainSampleSize = int(3000)
+# generate sample data
+trainSampleSize = int(100)#int(3000)
 print(f"Sample Size {trainSampleSize}")
-train_points = 2*np.random.rand(trainSampleSize,order)-1
+train_points = 2*np.random.rand(trainSampleSize, order)-1
 train_measures = legendre_measures(train_points, degree)
-augmented_train_measures = np.concatenate([train_measures, np.ones((1,trainSampleSize,degree+1))], axis=0)
+augmented_train_measures = np.concatenate(
+    [train_measures, np.ones((1, trainSampleSize, degree+1))], axis=0)
 
-f = lambda xs: np.linalg.norm(xs, axis=1)**2
+
+def f(xs): return Schloegel_ode.calc_end_reward(0, xs.T)#np.linalg.norm(xs, axis=1)**2
+
+
 end_values = f(train_points)
 
 
-#calculate local gramians
-localH1Gramians = [Gramian(degree+1,HkinnerLegendre(1)) for i in range(order)]
-localH1Gramians.append(np.ones([degree+1,degree+1]))
+# calculate local gramians
+localH1Gramians = [Gramian(degree+1, HkinnerLegendre(1)) for i in range(order)]
+localH1Gramians.append(np.ones([degree+1, degree+1]))
 localL2Gramians = [np.eye(degree+1) for i in range(order)]
-localL2Gramians.append(np.ones([degree+1,degree+1]))
+localL2Gramians.append(np.ones([degree+1, degree+1]))
 
 vlist = []
-#set end condititon
+# set end condititon
 
 
 for t in np.flipud(t_vec):
     print(f"Time point: {t}")
     if t == t_vec[-1]:
-        vend = random_homogenous_polynomial_sum([degree]*order, degree, maxGroupSize)
+        vend = random_homogenous_polynomial_sum(
+            [degree]*order, degree, maxGroupSize)
         vend.assume_corePosition(vend.order-1)
         while vend.corePosition > 0:
             vend.move_core('left')
         vend.components[0] = np.zeros(vend.components[0].shape)
         print(f"DOFS {vend.dofs()}")
-        solver = ALS(vend, augmented_train_measures,  end_values,_localL2Gramians=localL2Gramians,_localH1Gramians=localH1Gramians,_verbosity=1)
+        solver = ALS(vend, augmented_train_measures,  end_values,
+                     _localL2Gramians=localL2Gramians, _localH1Gramians=localH1Gramians, _verbosity=1)
         solver.maxSweeps = 10
         solver.targetResidual = 1e-8
         solver.run()
@@ -70,13 +83,12 @@ for t in np.flipud(t_vec):
     else:
         vnext = copy.deepcopy(vlist[-1])
         vlist.append(vnext)
-        
-        
+
         count = 0
         while count < maxPolIt:
-            #calculate rhs
+            # calculate rhs
             print(f"Start Calculating RHS")
-            rhs = calc_tilde_r(t,train_points.T,vlist)
+            rhs = calc_tilde_r(t, train_points.T, vlist)
             print(f"Finished Calculating RHS")
             values = vlist[-1].evaluate(augmented_train_measures)
             print(f"DOFS { vlist[-1].dofs()}")
@@ -85,11 +97,43 @@ for t in np.flipud(t_vec):
             print(f"Error {err}")
             if err < tol:
                 break
-            #update value function
-            solver = ALS(vlist[-1], augmented_train_measures,  rhs,_localL2Gramians=localL2Gramians,_localH1Gramians=localH1Gramians,_verbosity=1)
+            # update value function
+            solver = ALS(vlist[-1], augmented_train_measures,  rhs,
+                         _localL2Gramians=localL2Gramians, _localH1Gramians=localH1Gramians, _verbosity=1)
             solver.maxSweeps = maxSweeps
             solver.targetResidual = 1e-5
             solver.run()
-            count+=1
+            count += 1
+
+print("vlist", len(vlist))
+# evaluating the result of policy iteration
+x = 2*np.random.rand(1,order)-1
 
 
+#open loop solver from Leon
+step_size = .05
+step_size_before = 0.002
+max_iter = 1e5
+grad_tol = 1e-8
+steps = np.linspace(0, T, int(T/tau)+1)
+print('steps',len(steps),'tau',tau)
+m = len(steps)
+rew_hjb, u_hjb = calc_total_reward(x.T, steps,1*vlist)
+print('u_hjb',u_hjb.shape)
+control_dim = 1
+optimize_params = [step_size, step_size_before, max_iter, grad_tol]
+opt_u_item = optimize.Open_loop_solver(Schloegel_ode, optimize_params)
+opt_u_item.initialize_new(Schloegel_ode.calc_end_reward_grad, steps)
+
+def calc_opt(x0, u0, calc_cost):
+    
+    x_vec, u_vec = opt_u_item.calc_optimal_control(x0, u0)
+    cost = 1/2*calc_cost(0, x_vec[:, 0], u_vec[:, 0])
+    for i0 in range(len(steps)-1):
+        add_cost = calc_cost(steps[i0+1], x_vec[:,i0+1 ], u_vec[:, i0+1])
+        cost += add_cost
+    cost -= add_cost/2
+    cost += Schloegel_ode.calc_end_reward(0, x_vec[:,-1])
+    return x_vec.T, u_vec.T, cost
+x_opt, u_opt, cost_opt = calc_opt(x.T, u_hjb, Schloegel_ode.calc_reward)
+print("cost hjb", rew_hjb, 'cost opt', cost_opt)
--- hjb/optimize.py
+++ hjb/optimize.py
@@ -5,24 +5,25 @@ Created on Thu Feb  6 10:07:29 2020
 @author: sallandt
 """
 import numpy as np
-"""
-Calculates an optimal control using gradient descent of an optimal control problem
-\int_0^T x^t Q x + u^t R u dt
-where
-\dot x = A x + NL(x) + B u
-and T is the final time and t is the matrix transposed
-Variables:
-    A system matrix of size n,n
-    B system matrix of size n,control_dim
-    Q system matrix of size n,n
-    R system matrix of size control_dim,control_dim
-    tau time step in discretization of ODE
-    m Number of time steps, i.e. T = tau * m
-    n A.shape[1]
-    control_dim B.shape[1]
-    step is a function that solves the ODE, i.e.
-    x(tau) = step(x(0), u(0))
-"""
+
+#
+#Calculates an optimal control using gradient descent of an optimal control problem
+#\int_0^T x^t Q x + u^t R u dt
+#where
+#\dot x = A x + NL(x) + B u
+#and T is the final time and t is the matrix transposed
+#Variables:
+#    A system matrix of size n,n
+#    B system matrix of size n,control_dim
+ #   Q system matrix of size n,n
+ #   R system matrix of size control_dim,control_dim
+#    tau time step in discretization of ODE
+#    m Number of time steps, i.e. T = tau * m
+#    n A.shape[1]
+#    control_dim B.shape[1]
+#    step is a function that solves the ODE, i.e.
+#    x(tau) = step(x(0), u(0))
+
 
 import xerus as xe
 import numpy as np
@@ -41,6 +42,7 @@ class Open_loop_solver:
 
     def calc_optimal_control(self, x0, u0):
         u_opt = u0
+        print("u_opt", len(u_opt))
         grad1 = 0*u0
         i0 = 0
         curr_gradient_norm = self.grad_tol + 1
@@ -48,6 +50,7 @@ class Open_loop_solver:
             grad2 = self.gradJp(x0, u_opt)
             direction = -self.step_size*(grad2 + self.step_size_before*grad1)
             u_opt =  u_opt + direction
+            print('shapes in calc_optimal control grad2 t_steps',grad2.shape, self.t_steps.shape)
             curr_gradient_norm = np.sqrt(np.trapz(grad2**2, self.t_steps))
             i0 += 1
             if i0 % 100 == 0:
@@ -58,8 +61,9 @@ class Open_loop_solver:
 
 
     def solve_fixed_u(self, x0, u):
+        #print('slef.t_steps',self.t_steps)
         x= np.zeros((x0.size, len(self.t_steps)))
-        x[:,0] = x0
+        x[:,0] = x0[:,0]
         for i0 in range(len(self.t_steps)-1):
             x[:,i0+1] = self.ode.step(self.t_steps[i0], x[:,i0], u[:, i0])
         return x
@@ -69,12 +73,15 @@ class Open_loop_solver:
         ygrad = self.solve_fixed_u(x0, u)
         p = self.solve_peq(ygrad, u)
         c =  2*self.ode.R @ u +self.ode.B.T @ p
+        print('shapes in gradJp x0,u,p,c', x0.shape,u.shape,p.shape,c.shape)
         return c
 
 
     def solve_peq(self, x, u):
         p = np.zeros(shape=(x.shape[0], u.shape[1]))
         p[:,-1] = self.calc_final(self.t_steps[-1], x[:, -1])
+        
+       # print('p,x,t',p.shape,x.shape,self.t_steps.shape)
         for i0 in range(p.shape[1]-1, 0, -1):
             p[:, i0-1] = self.ode.step_adjoint(self.t_steps[i0], p[:,i0], x[:,i0])
     #        p[:, i0-1] = np.dot(A_tilde_T, p[:, i0] + tau*(x[:,i0] * (B.T @ p[:,i0]) + (1.5 - 0.15*x[:,i0])* np.exp(-0.1*x[:,i0]) * p[:,i0] + 2*Q@x[:,i0] ))
